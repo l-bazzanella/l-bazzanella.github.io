7da0750aefcff73d86a08bedfd7e345ca0dc6387
Mikke Nienow <34070014+mikkenienow@users.noreply.github.com>
2025-06-25 21:02:44 -0300

    Update lote.php

--- stamm/lote.php
+++ stamm/lote.php
@@ -10,11 +10,21 @@ $linhas = array_map("str_getcsv", explode("\n", $csv));
 
 // Pega cabeçalhos e última linha válida
 $headers = array_map('trim', $linhas[0]);
+debug_to_console($headers);
 $ultimaLinha = $linhas[count($linhas) - 2]; // penúltima, já que a última costuma estar vazia
-
+debug_to_console($ultimaLinha);
 // Converte para array associativo
 $resposta = array_combine($headers, $ultimaLinha);
 
 // Retorna o valor da coluna "Lote"
 header('Content-Type: application/json');
 echo json_encode(["lote" => $resposta["Lote"]]);
+
+
+function debug_to_console($data) {
+    $output = $data;
+    if (is_array($output))
+        $output = implode(',', $output);
+
+    echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
+}
